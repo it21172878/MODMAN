e54471c78024ae064c511c1c28575cb6e966b5a8
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-03-29 11:32:16 +0530

    for deployement

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import groupRoutes from './routes/projectGroupRoute.js';
 import cors from 'cors';
+import path from 'path';
 
 // dotenv config
 dotenv.config();
@@ -20,14 +21,18 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev')); // log request in
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/group', groupRoutes);
 
 // rest api
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to MODMAN</h1>');
+// app.get('/', (req, res) => {
+//   res.send('<h1>Welcome to MODMAN</h1>');
+// });
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 // port
