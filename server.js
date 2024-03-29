4c10e25e374b43bd828a90f076247fe1f9c40a9d
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-03-29 12:21:19 +0530

    back

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
 import groupRoutes from './routes/projectGroupRoute.js';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // dotenv config
 dotenv.config();
@@ -18,26 +16,18 @@ const app = express();
 // database config
 connectDB();
 
-// esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev')); // log request in
-app.use(express.static(path.join(__dirname, './client/public')));
 
 // routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/group', groupRoutes);
 
 // rest api
-// app.get('/', (req, res) => {
-//   res.send('<h1>Welcome to MODMAN</h1>');
-// });
-app.use('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to MODMAN</h1>');
 });
 
 // port
