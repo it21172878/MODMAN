1419b9f48cf8d1c27d33820161cbcc7745f2916b
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-04-11 20:18:57 +0530

    collection update

--- models/userOtp.js
+++ models/userOtp.js
@@ -1,26 +1,29 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 
-const userOtpSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Not Valid Email');
-      }
+const userOtpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Not Valid Email');
+        }
+      },
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
+    isVerify: {
+      type: Boolean,
+      default: false,
     },
   },
-  otp: {
-    type: String,
-    required: true,
-  },
-  isVerify: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 // user otp model
 const userotp = new mongoose.model('userotps', userOtpSchema);
